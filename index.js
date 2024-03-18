2c4a25f0812fcd5b721a222433c3e72df57fb888
bob <Mansalati18@gmail.com>
2024-03-18 11:31:48 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+describe('index.js', function () {
+    // there's stuff in here, too
+  });
+
+  it('is set as Scuber', function () {
+    // tests are here
+  });
+
+  it('is set as Scuber', function () {
+    expect(companyName).to.equal('Scuber');
+  });
+
+  let companyName = 'Scuber'
